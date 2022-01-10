059914ea215e68eb7249bee1e68e935409ef0bd1
youngjun12345 <ayj1810@naver.com>
2022-01-10 17:14:03 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -56,21 +56,7 @@ console.log("included main.js");
 // });
 
 
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-searchEl.addEventListener("click", function(){
-  searchInputEl.focus();
-});
 
-searchInputEl.addEventListener("focus", function(){
-  searchEl.classList.add("focused");
-  searchInputEl.setAttribute("placeholder", "통합검색");
-});
-
-searchInputEl.addEventListener("blur", function(){
-  searchEl.classList.remove("focused");
-  searchInputEl.setAttribute("placeholder", "");
-});
 
 const badgeEl = document.querySelector("header .badges");
 const toTopEl = document.querySelector("#to-top");
@@ -197,6 +183,3 @@ spyEls.forEach(function(spyEl){
     .setClassToggle(spyEl, "show")
     .addTo(new ScrollMagic.Controller());
 });
-
-const thisYear = document.querySelector(".this-year");
-thisYear.textContent = new Date().getFullYear();
